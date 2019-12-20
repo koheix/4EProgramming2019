c19f3222e31b8e5e78bb3c35a05ad2eaf7b61101
kohei <e1622@s.akashi.ac.jp>
2019-12-20 10:31:22 +0900

    12/20

--- UI.java
+++ UI.java
@@ -14,8 +14,10 @@ import java.util.Map;
 public class UI extends Application {
     private final int width = 960;
     private final int height = 540;//ウインドウの大きさ
+
+    static int[][] field = {{0 , 0 , 0},{0 , 0 , 0} , {0 , 0 , 0} , {0 , 0 , 0}};//盤面
  
-    /*Map<String, Integer>animals = new HashMap<String, Integer>() {
+    static Map<String, Integer>animals = new HashMap<String, Integer>() {
         {
             put("lion", 0);
             put("elephant", 1);
@@ -23,7 +25,7 @@ public class UI extends Application {
             put("chick" , 3);
             put("chicken" , 4);
         }
-    };*/
+    };
     public static void main(final String... args) {
         launch(args);
     }
@@ -44,12 +46,13 @@ public class UI extends Application {
         stage.show();//ウィンドウの表示
  
         drawField();
+        initialize(root);
     }
  
     private void drawField() {//描画のプログラム
         for(int i = 0;i < 4;i++){
             for(int j = 0;j < 3;j++){
-                g.strokeRect(
+                g.strokeRect(//盤面の左上は(285 , 10)
                     (width / 2) - ((height - 20) / 4)*3/2 + 130*j, 10 + 130*i,130, 130
                  );//盤面の描画//右上座標が100,20//盤面の描画
                 g.strokeRect(//もち米の枠
@@ -63,10 +66,14 @@ public class UI extends Application {
         }
     }
 
-    public void drawChara(String animal) {
+    public void drawChara(String animal , int x , int y ,Group root) {//x ,yは盤面の配列の座標
         switch(animal){//動物判定
             case "lion":
             //画像を貼る処理
+            ImageView lion = new ImageView("images/lion.png");
+            lion.setFitHeight(128);
+            lion.setFitWidth(128);
+            root.getChildren().add(lion);
                 break;
             case "elephant":
                 break;
@@ -79,5 +86,11 @@ public class UI extends Application {
             default:
                 break;
         }
+    }
+    private void initialize(Group root){//はじめに実行
+        drawChara("lion" , 3 , 1 , root);
+        drawChara("chick" , 2 , 1 , root);
+        drawChara("elephant" , 3 , 0 , root);
+        drawChara("giraffe", 3, 2 , root);
     }
 }
