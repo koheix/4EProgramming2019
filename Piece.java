43a311db2693d02cd8e4e98bda0754afd7362106
koheix <e1622.s.akashi.ac.jp>
2020-01-09 17:34:25 +0900

    Piece

--- Piece.java
+++ Piece.java
@@ -5,111 +5,72 @@ import java.util.Map;
 
 public class Piece {
     int directnum = 0;//すすめるマスの個数
-    Map<String, Boolean>direction = new HashMap<String, Boolean>() {//すすめるところを書く
-        {
-            //どうぶつの向きからみてその方向が行けるかどうかを格納してある。
-            put("fd", false);
-            put("bk", false);
-            put("right" , false);
-            put("left" , false);
-            put("rightfd" , false);
-            put("rightfd" , false);
-            put("leftfd" , false);
-            put("rightbk" , false);
-            put("rightbk" , false);
+    static boolean[][] direction = {{false , false , false},{false , false , false},{false , false , false}, {false, false , false}};//進めるところをtrueにする
+
+    public boolean movable(int x , int y){//正の向きのプレイヤーは1 , 負の向きは2
+        for(int i = 0; i < 4;i++){
+          for(int j = 0;j < 3;j++){
+            direction[i][j] = false;//リセット
+          }
         }
-    };
-    public boolean movable(int x , int y , int player){//正の向きのプレイヤーは1 , 負の向きは2
         int pieceX = (x - 285)/130; //field配列でのコマの位置
         int pieceY = (y - 10)/130;
-        setMovable(pieceX , pieceY , player);
+        setMovable(pieceX , pieceY);
         if(directnum >0) return true;//すすめるマスが一つでもあればtrueを返す
         else return false;
     }
-    public void move(String direction , int player){
-      if(player == 1){//正の向き
+    public void move(String direction){
 
-      }
-    };//選択されたコマが動く処理
-    public boolean isKing(){};//そのコマは王なのか
-    public boolean onBoard(int nextx , int nexty){
+    }//選択されたコマが動く処理
+    public boolean isKing(){
+        return false;
+    }//そのコマは王なのか
+    public boolean onBoard(int nextx , int nexty){//0 <= nextx <= 2 , 0 <= nexty <= 3
       if((0 <= nextx) && (nextx <= 2) && (0 <= nexty) && (nexty <= 3)) return true;
       else return false;
     }
-    public void setMovable(int pieceX , int pieceY , int player){
+    public void setMovable(int pieceX , int pieceY){
       directnum = 0;
       if((onBoard(pieceX , pieceY - 1)) && (Main.field[pieceY - 1][pieceX] <= 0)){//前はすすめる
-          if(player == 1) direction.put("fd", true);
-          else direction.put("bk" , true);
+          direction[pieceY - 1][pieceX] = true;
           directnum++;
       }
-      else{
-        if(player == 1) direction.put("fd", false);
-        else direction.put("bk" , false);
-      }
+
       if((onBoard(pieceX + 1 , pieceY - 1)) && (Main.field[pieceY - 1][pieceX + 1] <= 0)){//rightfdは進める
-        if(player == 1) direction.put("rightfd" , true);
-        else direction.put("leftbk" , true);
-        directnum++;
+          direction[pieceY - 1][pieceX + 1] = true;
+          directnum++;
       }
-      else{
-        if(player == 1) direction.put("rightfd" , false);
-        else direction.put("leftbk" , false);
-      }
+
       if((onBoard(pieceX + 1 , pieceY)) && (Main.field[pieceY][pieceX + 1] <= 0)){//rightは進める
-        if(player == 1) direction.put("right" , true);
-        else direction.put("left" , true);
+        direction[pieceY][pieceX + 1] = true;
         directnum++;
       }
-      else{
-        if(player == 1) direction.put("right" , false);
-        else direction.put("left" , false);
-      }
+
       if((onBoard(pieceX + 1 , pieceY + 1)) && (Main.field[pieceY + 1][pieceX + 1] <= 0)){//rightbkは進める
-        if(player == 1) direction.put("rightbk" , true);
-        else direction.put("leftfd" , true);
+        direction[pieceY + 1][pieceX + 1] = true;
         directnum++;
       }
-      else{
-        if(player == 1) direction.put("rightbk" , false);
-        else direction.put("leftfd" , false);
-      }
+
       if((onBoard(pieceX , pieceY + 1)) && (Main.field[pieceY + 1][pieceX] <= 0)){//bkは進める
-        if(player == 1) direction.put("bk" , true);
-        else direction.put("fd" , true);
+        direction[pieceY + 1][pieceX] = true;
         directnum++;
       }
-      else{
-        if(player == 1) direction.put("bk" , false);
-        else direction.put("fd" , false);
-      }
+
       if((onBoard(pieceX - 1 , pieceY + 1)) && (Main.field[pieceY + 1][pieceX - 1] <= 0)){//leftbkは進める
-        if(player == 1) direction.put("leftbk" , true);
-        else direction.put("rightfd" , true);
+        direction[pieceY + 1][pieceX - 1] = true;
         directnum++;
       }
-      else{
-        if(player == 1) direction.put("leftbk" , false);
-        else direction.put("rightfd" , false);
-      }
+
       if((onBoard(pieceX - 1 , pieceY)) && (Main.field[pieceY][pieceX - 1] <= 0)){//leftは進める
-        if(player == 1) direction.put("left" , true);
-        else direction.put("right" , true);
+        direction[pieceY][pieceX - 1] = true;
         directnum++;
       }
-      else{
-        if(player == 1) direction.put("left" , false);
-        else direction.put("right" , false);
-      }
+
       if((onBoard(pieceX - 1 , pieceY - 1)) && (Main.field[pieceY - 1][pieceX - 1] <= 0)){//leftfdは進める
-        if(player == 1) direction.put("leftfd" , true);
-        else direction.put("rightbk" , true);
+        direction[pieceY - 1][pieceX - 1] = true;
         directnum++;
       }
-      else{
-        if(player == 1) direction.put("leftfd" , false);
-        else direction.put("rightbk" , false);
-      }
+
     }
 }
 
@@ -118,4 +79,15 @@ class Lion extends Piece{
     public boolean isKing(){
         return true;
     }
+}
+
+class Giraffe extends Piece{
+  @Override
+  public void setMovable(int pieceX , int pieceY){
+    super.setMovable(pieceX, pieceY);
+    direction[pieceY - 1][pieceX + 1] = false;
+    direction[pieceY + 1][pieceX + 1] = false;
+    direction[pieceY + 1][pieceX - 1] = false;
+    direction[pieceY - 1][pieceX - 1] = false;
+  }
 }
