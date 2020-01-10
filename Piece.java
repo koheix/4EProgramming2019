cadefbdd01f4e1619e0cffcef025e90f16297744
kohei <e1622@s.akashi.ac.jp>
2020-01-10 09:49:01 +0900

    fixed bugs

--- Piece.java
+++ Piece.java
@@ -5,16 +5,19 @@ public class Piece {
     int directnum = 0;//すすめるマスの個数
     static boolean[][] direction = {{false , false , false},{false , false , false},{false , false , false}, {false, false , false}};//進めるところをtrueにする
 
-    public boolean movable(int x , int y){//正の向きのプレイヤーは1 , 負の向きは2
-        for(int i = 0; i < 4;i++){
-          for(int j = 0;j < 3;j++){
-            direction[i][j] = false;//リセット
-          }
+    static void directionReset(){//direction配列をリセットする
+      for(int i = 0; i < 4;i++){
+        for(int j = 0;j < 3;j++){
+          direction[i][j] = false;//リセット
         }
+      }
+    }
+    public boolean movable(int x , int y , int player){//正の向きのプレイヤーは1 , 負の向きは2
+        directionReset();
         int pieceX = (x - 285)/130; //field配列でのコマの位置
         int pieceY = (y - 10)/130;
         if(!onBoard(pieceX, pieceY)) return false;//クリックされた場所が盤面上ではない
-        setMovable(pieceX , pieceY);
+        setMovable(pieceX , pieceY, player);
         if(directnum >0) return true;//すすめるマスが一つでもあればtrueを返す
         else return false;
     }
@@ -28,7 +31,7 @@ public class Piece {
       if((0 <= nextx) && (nextx <= 2) && (0 <= nexty) && (nexty <= 3)) return true;
       else return false;
     }
-    public void setMovable(int pieceX , int pieceY){
+    public void setMovable(int pieceX , int pieceY, int player){
       directnum = 0;
       if((onBoard(pieceX , pieceY - 1)) && (Main.field[pieceY - 1][pieceX] <= 0)){//前はすすめる
           direction[pieceY - 1][pieceX] = true;
@@ -82,30 +85,47 @@ class Lion extends Piece{
 
 class Giraffe extends Piece{
   @Override
-  public void setMovable(int pieceX , int pieceY){
-    super.setMovable(pieceX, pieceY);
-    direction[pieceY - 1][pieceX + 1] = false;
-    direction[pieceY + 1][pieceX + 1] = false;
-    direction[pieceY + 1][pieceX - 1] = false;
-    direction[pieceY - 1][pieceX - 1] = false;
+  public void setMovable(int pieceX , int pieceY, int player){
+    super.setMovable(pieceX, pieceY, player);
+    if(onBoard(pieceY - 1, pieceX + 1)) direction[pieceY - 1][pieceX + 1] = false;
+    if(onBoard(pieceY + 1, pieceX + 1)) direction[pieceY + 1][pieceX + 1] = false;
+    if(onBoard(pieceY + 1, pieceX - 1)) direction[pieceY + 1][pieceX - 1] = false;
+    if(onBoard(pieceY - 1, pieceX - 1)) direction[pieceY - 1][pieceX - 1] = false;
   }
 }
 
 class Elephant extends Piece{
   @Override
-  public void setMovable(int pieceX, int pieceY){
-    super.setMovable(pieceX, pieceY);
-    direction[pieceY - 1][pieceX] = false;
-    direction[pieceY][pieceX + 1] = false;
-    direction[pieceY + 1][pieceX] = false;
-    direction[pieceY][pieceX - 1] = false;
+  public void setMovable(int pieceX, int pieceY, int player){
+    super.setMovable(pieceX, pieceY, player);
+    if(onBoard(pieceY - 1, pieceX)) direction[pieceY - 1][pieceX] = false;
+    if(onBoard(pieceY, pieceX + 1)) direction[pieceY][pieceX + 1] = false;
+    if(onBoard(pieceY + 1, pieceX)) direction[pieceY + 1][pieceX] = false;
+    if(onBoard(pieceY, pieceX - 1)) direction[pieceY][pieceX - 1] = false;
   }
 }
 
 class Chick extends Piece{
   @Override
-  public void setMovable(int pieceX, int pieceY){
-    super.setMovable(pieceX, pieceY);
-
+  public void setMovable(int pieceX, int pieceY, int player){
+    super.setMovable(pieceX, pieceY, player);
+    if(player == 1){//前向きのプレイヤー
+    direction[pieceY][pieceX + 1] = false;
+    direction[pieceY + 1][pieceX] = false;
+    direction[pieceY][pieceX - 1] = false;
+    direction[pieceY - 1][pieceX + 1] = false;
+    direction[pieceY + 1][pieceX + 1] = false;
+    direction[pieceY + 1][pieceX - 1] = false;
+    direction[pieceY - 1][pieceX - 1] = false;
+    }
+    else{//後ろ向きのプレイヤー
+    direction[pieceY - 1][pieceX] = false;
+    direction[pieceY][pieceX + 1] = false;
+    direction[pieceY][pieceX - 1] = false;
+    direction[pieceY - 1][pieceX + 1] = false;
+    direction[pieceY + 1][pieceX + 1] = false;
+    direction[pieceY + 1][pieceX - 1] = false;
+    direction[pieceY - 1][pieceX - 1] = false;
+    }
   }
 }
